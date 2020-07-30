97b1fd9791f53f972e79845ab7b336199d139bcd
Akshata-magadum <68121583+Akshata-magadum@users.noreply.github.com>
2020-07-30 16:49:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+(function(){
+var names=["yakov","John",
+"Jen","jason","paul","frank","larry","brian","jim"];
+for(var i=0;i<names.length;i++){
+  var firstletter=names[i].charAt(0).toLowercase();
+if(firstletter=="j"){byespeaker.speak(name[i]);}
+else{hello>speaker.speak(name[i]);}
+}})();
---
+++ speakhello.js
@@ -0,0 +1,7 @@
+(function (window){
+var hellospeaker={}
+var speakword="Hello";
+hellospeaker.speak=function(name){
+   console.log(speakword+" "+name);}
+window.hellospeaker=hellospeaker;
+})(window);
